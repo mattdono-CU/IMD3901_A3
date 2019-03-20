ce43f8d80ad547eb15f37d9821aad78041707701
mattdono-CU <matthew.m.donoghue@gmail.com>
2019-03-20 11:59:30 -0400

    queue and proper detection of both buttons being pressed

--- app.js
+++ app.js
@@ -5,6 +5,10 @@ const http      = require('http');
 const server    = http.createServer(app);
 const socketIO  = require('socket.io')(server);
 
+let buttonA     = false;
+let buttonB     = false;
+let users       = 0;
+
 //const vars
 const LISTEN_PORT = 8080;
 
@@ -27,99 +31,218 @@ app.get('/winner', function(req, res) {
 app.get('/loser', function(req, res) {
     res.sendFile(__dirname + 'public/loser.html');
 });
+app.get('/queue', function(req, res) {
+    res.sendFile(__dirname + 'public/queue.html');
+});
 
 //S O C K E T   C O D E
-socketIO.on('connection', function(socket) {    
-    let buttonA     = false;
-    let matchListA  = [];
-    let buttonB     = false;
-    let matchListB  = [];
-    let reveal      = false;
+//User - 1
+if (users === 0) {
+    socketIO.on('connection', function(socket) {
+        console.log('[' + socket.id + ' connected]')
 
-    console.log('[' + socket.id + ' connected]')
+        users += 1;
+        console.log('Users: ' + users);
+        socketIO.sockets.emit('connected', users);
 
-    socket.on('disconnect', function() {
-        console.log('[' + socket.id + ' disconnected]')
-    });
-
-    ///////////////////////////
-    // R O O M   E V E N T S //
-    ///////////////////////////
-    //Console Button Pressed
-    socket.on('buttonpressed', function(roomId) {
-        if (roomId === "A") {
-            buttonA = true;
-        } else if (roomId === "B") {
-            buttonB = true;
-        }
-
-        console.log('[Button A: ' + buttonA + ' | Button B: ' + buttonB + ']');
-
-        if (buttonA && buttonB) {
-            console.log('[Reveal Registered]');
-            reveal = true;
-            socketIO.sockets.emit('reveal');
-        } else {
-            console.log('[ColourShft @ Room' + roomId + ']');
-            socketIO.sockets.emit('colourshift');
-        }
-    });
-    //Console Button Released
-    socket.on('buttonreleased', function(roomId) {
-        if (roomId === "A") {
-            buttonA = false;
-        } else if (roomId === "B") {
-            buttonB = false;
-        }
-        
-        console.log('[Button A: ' + buttonA + ' | Button B: ' + buttonB + ']');
-
-        if (reveal) {
-            reveal = false;
-            socketIO.sockets.emit('colourshift');
-        }
-    });
-    //Block + Pad Match Found
-    socket.on('matchfound', function(roomId, blockId) {
-        console.log('Match Found: ' + blockId + ' @ ' + roomId);
-        
-        if (roomId === "A") {
-            if (!matchListA.includes(blockId)) {
-                matchListA.push(blockId);
+        let matchListA  = [];
+        let matchListB  = [];
+        let reveal      = false;
+    
+        socket.on('disconnect', function() {
+            console.log('[' + socket.id + ' disconnected]')
+            
+            users -= 1;
+            console.log('Users: ' + users);
+            socketIO.sockets.emit('disconnected', users);
+        });
+    
+        ///////////////////////////
+        // R O O M   E V E N T S //
+        ///////////////////////////
+        //Console Button Pressed
+        socket.on('buttonpressed', function(roomId) {
+            console.log(roomId);
+            if (roomId === "A") {
+                buttonA = true;
+                console.log('buttonA: ' + buttonA);
+            } else if (roomId === "B") {
+                buttonB = true;
+                console.log('buttonB: ' + buttonB);
+            }
+    
+            console.log('[Button A: ' + buttonA + ' | Button B: ' + buttonB + ']');
+    
+            if (buttonA && buttonB) {
+                console.log('[Reveal Registered]');
+                reveal = true;
+                socketIO.sockets.emit('reveal');
+            } else {
+                console.log('[ColourShft @ Room' + roomId + ']');
+                socketIO.sockets.emit('colourshift');
+            }
+        });
+        //Console Button Released
+        socket.on('buttonreleased', function(roomId) {
+            if (roomId === "A") {
+                buttonA = false;
+            } else if (roomId === "B") {
+                buttonB = false;
+            }
+            
+            console.log('[Button A: ' + buttonA + ' | Button B: ' + buttonB + ']');
+    
+            if (reveal) {
+                reveal = false;
+                socketIO.sockets.emit('colourshift');
+            }
+        });
+        //Block + Pad Match Found
+        socket.on('matchfound', function(roomId, blockId) {
+            console.log('Match Found: ' + blockId + ' @ ' + roomId);
+            
+            if (roomId === "A") {
+                if (!matchListA.includes(blockId)) {
+                    matchListA.push(blockId);
+                    console.log(matchListA);
+                }
+                if (matchListA.length === 4) {
+                    console.log("Room A Wins!");
+                    socketIO.sockets.emit('gameover', 'A');
+                }
+            } else if (roomId === "B") {
+                if (!matchListB.includes(blockId)) {
+                    matchListB.push(blockId);
+                    console.log(matchListB);
+                }
+                if (matchListB.length === 4) {
+                    console.log("Room B Wins!");
+                    socketIO.sockets.emit('gameover', 'B');
+                }
+            }
+        });
+        //Block + Pad Match Lost
+        socket.on('matchlost', function(roomId, blockId) {
+            console.log('Match Lost: ' + blockId + ' @ ' + roomId);
+            
+            if (roomId === "A") {
+                if (matchListA.includes(blockId)) {
+                    matchListA.splice(matchListA.indexOf(blockId), 1);
+                }
                 console.log(matchListA);
-            }
-            if (matchListA.length === 4) {
-                console.log("Room A Wins!");
-                socketIO.sockets.emit('gameover', 'A');
-            }
-        } else if (roomId === "B") {
-            if (!matchListB.includes(blockId)) {
-                matchListB.push(blockId);
+            } else if (roomId === "B") {
+                if (matchListA.includes(blockId)) {
+                    matchListA.splice(matchListA.indexOf(blockId), 1);
+                }
                 console.log(matchListB);
             }
-            if (matchListB.length === 4) {
-                console.log("Room B Wins!");
-                socketIO.sockets.emit('gameover', 'B');
-            }
-        }
+        });
     });
-    //Block + Pad Match Lost
-    socket.on('matchlost', function(roomId, blockId) {
-        console.log('Match Lost: ' + blockId + ' @ ' + roomId);
-        
-        if (roomId === "A") {
-            if (matchListA.includes(blockId)) {
-                matchListA.splice(matchListA.indexOf(blockId), 1);
+}
+
+//User - 2
+else if (users > 0) {
+    socketIO.on('connection', function(socket) {
+        console.log('[' + socket.id + ' connected]')
+
+        users += 1;
+        console.log('Users: ' + users);
+        socketIO.sockets.emit('connected', users);
+
+        let matchListA  = [];
+        let matchListB  = [];
+        let reveal      = false;
+    
+        socket.on('disconnect', function() {
+            console.log('[' + socket.id + ' disconnected]')
+
+            users -= 1;
+            console.log('Users: ' + users);
+            socketIO.sockets.emit('disconnected', users);
+        });
+    
+        ///////////////////////////
+        // R O O M   E V E N T S //
+        ///////////////////////////
+        //Console Button Pressed
+        socket.on('buttonpressed', function(roomId) {
+            console.log(roomId);
+            if (roomId === "A") {
+                buttonA = true;
+                console.log('buttonA: ' + buttonA);
+            } else if (roomId === "B") {
+                buttonB = true;
+                console.log('buttonB: ' + buttonB);
             }
-            console.log(matchListA);
-        } else if (roomId === "B") {
-            if (matchListA.includes(blockId)) {
-                matchListA.splice(matchListA.indexOf(blockId), 1);
+    
+            console.log('[Button A: ' + buttonA + ' | Button B: ' + buttonB + ']');
+    
+            if (buttonA && buttonB) {
+                console.log('[Reveal Registered]');
+                reveal = true;
+                socketIO.sockets.emit('reveal');
+            } else {
+                console.log('[ColourShft @ Room' + roomId + ']');
+                socketIO.sockets.emit('colourshift');
             }
-            console.log(matchListB);
-        }
+        });
+        //Console Button Released
+        socket.on('buttonreleased', function(roomId) {
+            if (roomId === "A") {
+                buttonA = false;
+            } else if (roomId === "B") {
+                buttonB = false;
+            }
+            
+            console.log('[Button A: ' + buttonA + ' | Button B: ' + buttonB + ']');
+    
+            if (reveal) {
+                reveal = false;
+                socketIO.sockets.emit('colourshift');
+            }
+        });
+        //Block + Pad Match Found
+        socket.on('matchfound', function(roomId, blockId) {
+            console.log('Match Found: ' + blockId + ' @ ' + roomId);
+            
+            if (roomId === "A") {
+                if (!matchListA.includes(blockId)) {
+                    matchListA.push(blockId);
+                    console.log(matchListA);
+                }
+                if (matchListA.length === 4) {
+                    console.log("Room A Wins!");
+                    socketIO.sockets.emit('gameover', 'A');
+                }
+            } else if (roomId === "B") {
+                if (!matchListB.includes(blockId)) {
+                    matchListB.push(blockId);
+                    console.log(matchListB);
+                }
+                if (matchListB.length === 4) {
+                    console.log("Room B Wins!");
+                    socketIO.sockets.emit('gameover', 'B');
+                }
+            }
+        });
+        //Block + Pad Match Lost
+        socket.on('matchlost', function(roomId, blockId) {
+            console.log('Match Lost: ' + blockId + ' @ ' + roomId);
+            
+            if (roomId === "A") {
+                if (matchListA.includes(blockId)) {
+                    matchListA.splice(matchListA.indexOf(blockId), 1);
+                }
+                console.log(matchListA);
+            } else if (roomId === "B") {
+                if (matchListA.includes(blockId)) {
+                    matchListA.splice(matchListA.indexOf(blockId), 1);
+                }
+                console.log(matchListB);
+            }
+        });
     });
-});
+}
 
 //start server
 server.listen(LISTEN_PORT);
